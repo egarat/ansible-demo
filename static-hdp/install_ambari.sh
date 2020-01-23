fbaab2537e35a35590e8497b839e1f348d542085
Kaoroprat Egarat, B2B-BPN-SAP-ANY-ANA <Egarat.Kaoroprat@swisscom.com>
2020-01-23 14:56:15 +0100

    Remove unnecessary files

--- static-hdp/install_ambari.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source $(dirname "${BASH_SOURCE[0]}")/set_cloud.sh
-source $(dirname "${BASH_SOURCE[0]}")/set_inventory.sh
-
-ansible-playbook "playbooks/install_ambari.yml" \
-                 --inventory="inventory/${inventory_to_use}" \
-                 --extra-vars="cloud_name=${cloud_to_use}" \
-                 "$@"
